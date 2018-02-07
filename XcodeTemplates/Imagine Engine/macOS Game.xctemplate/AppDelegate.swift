387f3c2c99cbc6a15434b93856596f3fc5db61d4
Tyrfing Mjolner <4lphamal3@gmail.com>
2018-02-07 18:09:23 +0100

    %s/IU/NS/g
    %s/IU/NS/g

--- XcodeTemplates/Imagine Engine/macOS Game.xctemplate/AppDelegate.swift
+++ XcodeTemplates/Imagine Engine/macOS Game.xctemplate/AppDelegate.swift
@@ -1,11 +1,11 @@
 import AppKit
 import ImagineEngine
 
-@UIApplicationMain class AppDelegate: NSResponder, NSApplicationDelegate {
+@NSApplicationMain class AppDelegate: NSResponder, NSApplicationDelegate {
     var window: NSWindow?
 
-    func application(_ application: NSApplication, didFinishLaunchingWithOptions launchOptions: [NSApplicationLaunchOptionsKey: Any]?) -> Bool {
-        let scene = ___PACKAGENAME___Scene(size: UIScreen.main.bounds.size)
+    func application(_ application: NSApplication, didFinishLaunchingWithOptions launchOptions: [NApplicationLaunchOptionsKey: Any]?) -> Bool {
+        let scene = ___PACKAGENAME___Scene(size: NSScreen.main.bounds.size)
 
         let window = GameWindow(scene: scene)
         window.makeKeyAndVisible()
